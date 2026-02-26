ebbda6ffb7518db4f313aca5233988a5614eea66
Anuj7104 <150335293+Anuj7104@users.noreply.github.com>
2026-02-26 09:35:48 +0530

    Create config.py with database configuration.

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+DB_CONFIG = {
+    'host': os.getenv("DB_HOST", "localhost"),
+    'user': os.getenv("DB_USER", "root"),
+    'password': os.getenv("DB_PASSWORD", "yourpassword"),
+    'database': os.getenv("DB_NAME", "flight_db")
+}
